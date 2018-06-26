e55ded827d3e18771b333b1968918370b1f1ad6f
Jean Carlo da Silva <jeaan3@gmail.com>
2018-06-26 01:36:35 -0300

    fix(api): adjusting api URL

--- BestSeries/Infrastructure/Api/IApiRestful.cs
+++ BestSeries/Infrastructure/Api/IApiRestful.cs
@@ -9,7 +9,7 @@ namespace BestSeries.Infrastructure.Api
     [Headers("Content-Type: application/json")]
     public interface IApiRestful
     {
-        [Get("https://api.themoviedb.org/3/tv/popular?api_key={apiKey}")]
+        [Get("/3/tv/popular?api_key={apiKey}")]
         Task<IEnumerable<Serie>> GetSeriesAsync(string apiKey);
     }
 }
--- BestSeries/Infrastructure/Helpers/ConstantHelper.cs
+++ BestSeries/Infrastructure/Helpers/ConstantHelper.cs
@@ -3,7 +3,7 @@ namespace BestSeries.Infrastructure.Helpers
 {
         public static class ConstantHelper
         {
-            public const string ApiUrl = "https://api.themoviedb.org";
+            public const string ApiUrl = "https://api.themoviedb.org"; 
             public const string ApiKey = "1f54bd990f1cdfb230adb312546d765d";
             public const string ImageUrlPref = "https://image.tmdb.org/t/p/w500";
         }
